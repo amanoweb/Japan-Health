57e045477d8bfe215da2ba64e2cc60d3cd1e3740
amanoweb <tomoyukiamano0@gmail.com>
2026-09-05 23:02:31 +0900

    Load service access promotions in provider API

--- api/providers.js
+++ api/providers.js
@@ -17,7 +17,8 @@ const STATIC_PROVIDER_FILES=[
   "data/provider-promotions-batch-7.js",
   "data/provider-promotions-batch-8.js",
   "data/provider-promotions-batch-9.js",
-  "data/provider-promotions-batch-10.js"
+  "data/provider-promotions-batch-10.js",
+  "data/service-access-promotions-2026-09-05.js"
 ];
 
 function validProvider(p){
